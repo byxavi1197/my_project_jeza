3fb0669c58440916858de90633b905d02d0fcb01
JavierZamora <javierzamora@gmail.com>
2025-10-08 00:22:08 -0600

    Strategy JWT

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { LoginDto } from "./dto/login.dto";
+import {AuthService} from './auth.service';
+import { Body, Controller, HttpException, HttpStatus, Post } from "@nestjs/common";
+
+@Controller('auth')
+export class AuthController {
+
+constructor(private authService: AuthService) {}
+
+@Post('login') 
+async login(
+    @Body() data: LoginDto 
+) {
+
+const usertoken = await this.authService.validateUser(data);
+
+if (!usertoken) throw new HttpException('Invalid credentials', HttpStatus. UNAUTHORIZED);
+
+return usertoken;
+}
+}
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const config = new DocumentBuilder()
 
-.setTitle('My API')
-.setDescription('API description')
-.setVersion('1.0')
-.addTag('api')
-.build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+    .setTitle('My API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+      name: 'Authorization',
+      description: 'Enter your bearer token',
+    })
+    .addSecurityRequirements('bearer')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
